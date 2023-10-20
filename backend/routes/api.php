09bbc84e9d7322f0cb7288de3574c9cd0cc3fe01
timkrysta <krystatymoteusz@gmail.com>
2023-10-20 12:52:14 +0200

    [#3] Created api endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+// [GET] /articles - listowanie wszystkich artykułów
+Route::get('/articles', [ArticleController::class, 'index']);
+
+// [GET] /article/:guid - szczegóły jednego artykułu
+Route::get('/article/{article:id}', [ArticleController::class, 'show']);
