eabe3bb9d8dc36081ffe3b05d7790e853f4a69ec
timkrysta <krystatymoteusz@gmail.com>
2023-10-21 22:19:46 +0200

    [#8] Updated styling

--- frontend/src/modules/article/ArticlesIndex/ArticlesIndex.vue
+++ frontend/src/modules/article/ArticlesIndex/ArticlesIndex.vue
@@ -42,10 +42,50 @@ watch(search, () => {
 
 
 <template>
-    <div>
-        <input type="search" placeholder="Search by title or category" v-model.trim="search" />
-        <main>
-            <ArticlePreview v-for="article in filteredArticles" :key="article.id" :article="article" />
-        </main>
-    </div>
+    <main>
+        <h1>Artykuły</h1>
+        <div class="card mb-3">
+            <div class="searchInput outlined">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="#6c757d" style="margin-right: .5rem;">
+                    <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                </svg>
+                <input type="search" placeholder="Search by title or category" v-model.trim="search" />
+            </div>
+        </div>
+        <ArticlePreview v-for="article in filteredArticles" :key="article.id" :article="article" />
+    </main>
 </template>
+
+
+<style scoped>
+.searchInput {
+    display: flex;
+    align-items: center;
+    border: 1px solid #ced4da;
+    border-radius: 0.5rem;
+    padding: 0.375rem 0.75rem;
+    background-color: #fff;
+}
+
+.searchInput input {
+    border: none;
+    outline: none;
+    background-color: transparent;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+}
+
+.searchInput svg {
+    color: #6c757d;
+    margin-right: 0.5rem;
+}
+
+.outlined {
+    outline-offset: 2px;
+    transition: outline 0.2s ease-in-out;
+}
+.outlined:focus-within {
+    outline: 3px solid #007BFF;
+}
+</style>
--- frontend/src/modules/article/ArticlesIndex/components/ArticlePreview.vue
+++ frontend/src/modules/article/ArticlesIndex/components/ArticlePreview.vue
@@ -8,37 +8,55 @@ defineProps<Props>();
 </script>
 
 <template>
-    <router-link :to="`/article/${article.id}`" class="article">
-        <h5>{{ article.title }}</h5>
-        <div>{{ article.pubDate }}</div>
-        <div class="badge-container">
+    <router-link :to="`/article/${article.id}`" class="card mb-3">
+        <h2 class="title mb-2">{{ article.title }}</h2>
+        <div class="badge-container mb-2">
             <template v-for="category in article.category" :key="category">
                 <span class="badge">{{ category }}</span>
             </template>
         </div>
-        <div>{{ article.description }}</div>
+        <div class="lead">{{ article.description }}</div>
+        <div class="publishDate">Opublikowano: {{ article.pubDate }}</div>
     </router-link>
 </template>
 
 <style scoped>
-.article {
-    margin-bottom: 1rem;
-    border: 1px solid black;
-    border-radius: 10px;
-    padding: 1rem;
-    display: block;
-}
 .badge-container {
     display: flex;
     gap: .5rem;
 }
+
 .badge {
-  display: inline-block;
-  padding: 4px 8px;
-  border-radius: 4px;
-  font-size: 12px;
-  font-weight: bold;
-  background-color: #007bff;
-  color: #fff;
+    display: inline-block;
+    padding: 4px 8px;
+    border-radius: 4px;
+    font-size: 12px;
+    font-weight: bold;
+    background-color: #007bff;
+    color: #fff;
+}
+
+.card:hover {
+    background-color: #f3f3f3;
+}
+
+.title {
+    margin-top: 0;
+}
+
+.lead,
+.publishDate {
+    color: #6c757d;
+}
+
+.lead {
+  font-size: .9rem;
+  font-weight: 300;
+  line-height: 1.6;
+}
+.publishDate {
+    font-size: .6rem;
+    margin-top: .25rem;
+    text-align: end;
 }
 </style>
